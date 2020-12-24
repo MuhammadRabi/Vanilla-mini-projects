07482d0423d830c282cbccbf0c5afb426b8a7b68
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2020-12-24 12:22:34 +0200

    mini projects js
    John Smigla tautorials

---
+++ 1-color generator/app1.js
@@ -0,0 +1,34 @@
+    // creaing array of colors
+let hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+let specialBtn = document.getElementById('btn-generate');
+    colorText = document.querySelector('.color');
+
+    // add eventlistener on button
+
+specialBtn.addEventListener('click', (e)=>{
+
+    let hexColor = '#';
+
+    // loop for 6 digits for hex code
+    for (i = 0; i < 6; i++){
+
+        hexColor += hexArr[generateColor()];
+
+    }
+
+    // changing the background color of page 
+    document.body.style.backgroundColor = hexColor;
+
+    // writing down the hex code of generated color
+    colorText.textContent = hexColor;
+
+})
+
+
+
+    function generateColor (){
+      return Math.floor(Math.random() * hexArr.length);
+    }
+
