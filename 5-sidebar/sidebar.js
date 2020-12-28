6a674d66ccbd456479ec6e342c05f1c59b5db848
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2020-12-28 15:07:56 +0200

    responsive
    responsive fixes + animation toggle btn

--- 5-sidebar/sidebar.js
+++ 5-sidebar/sidebar.js
@@ -1,3 +1,18 @@
 //.sidebar-toggle
 //.close-btn
 //.show-sidebar
+
+const sideBar = document.querySelector(".sidebar");
+const sideToggle = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+
+// add a class showing the sidebar or remove it if existed
+
+sideToggle.addEventListener("click", () => {
+  sideBar.classList.toggle("show-sidebar");
+});
+
+// clicking close btn will hide the sideBar again
+closeBtn.addEventListener("click", () => {
+  sideBar.classList.remove("show-sidebar");
+});
